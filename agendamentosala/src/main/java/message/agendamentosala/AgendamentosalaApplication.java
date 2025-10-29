d712dabb05579f4b25d1f96b12e37d00f11a8337
kaio10-collab <sancheskaio@gmail.com>
2025-10-29 14:01:58 -0300

    Primeiro commit do meu projeto

---
+++ agendamentosala/src/main/java/message/agendamentosala/AgendamentosalaApplication.java
@@ -0,0 +1,13 @@
+package message.agendamentosala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentosalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentosalaApplication.class, args);
+	}
+
+}
---
+++ agendamentosala/src/test/java/message/agendamentosala/AgendamentosalaApplicationTests.java
@@ -0,0 +1,13 @@
+package message.agendamentosala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentosalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
