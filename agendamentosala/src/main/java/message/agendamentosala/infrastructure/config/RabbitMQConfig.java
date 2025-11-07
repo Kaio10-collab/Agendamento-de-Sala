a38134d8fa0d1a3509f780e0a3812ed04d916758
kaio10-collab <sancheskaio@gmail.com>
2025-11-07 14:27:34 -0300

    Refatorando classe de configuracao do rabbit.

--- agendamentosala/src/main/java/message/agendamentosala/infrastructure/config/RabbitMQConfig.java
+++ agendamentosala/src/main/java/message/agendamentosala/infrastructure/config/RabbitMQConfig.java
@@ -1,77 +1,61 @@
 package message.agendamentosala.infrastructure.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Configuration
-@EnableRabbit
 public class RabbitMQConfig {
 
-    public static final String SCHEDULING_EXCHANGE = "room.scheduling.exchange";
-    public static final String CHECK_IN_ROUTING_KEY = "room.scheduling.checkin";
-    public static final String CANCELLATION_QUEUE = "room.scheduling.cancellation";
-    public static final String DELAY_EXCHANGE = "room.scheduling.delay.exchange";
-    public static final String DELAY_QUEUE = "room.scheduling.delay.queue";
+    public static final String STAND_BY_DELAY_ROUTING_KEY = "standby.delay";
+    public static final String STAND_BY_CANCEL_ROUTING_KEY = "standby.cancel";
+    public static final String STAND_BY_DELAY_EXCHANGE = "room.standby.delay.exchange";
+    public static final String STAND_BY_DELAY_QUEUE = "room.standby.delay.queue";
+    public static final String STAND_BY_DLX_EXCHANGE = "room.standby.dlx.exchange";
+    public static final String STAND_BY_CANCEL_QUEUE = "room.standby.cancel.queue";
+    public static final long STAND_BY_TTL_MS = 15 * 60 * 1000;
+
+    // --- 1. Exchanges ---
 
     @Bean
-    public DirectExchange schedulingExchange() {
-        return new DirectExchange(SCHEDULING_EXCHANGE);
+    public Exchange standbyDelayExchange() {
+        return ExchangeBuilder.directExchange(STAND_BY_DELAY_EXCHANGE).build();
     }
 
     @Bean
-    public DirectExchange delayExchange() {
-        return new DirectExchange(DELAY_EXCHANGE);
+    public Exchange standbyDlxExchange() {
+        return ExchangeBuilder.directExchange(STAND_BY_DLX_EXCHANGE).build();
     }
 
-    @Bean
-    public Queue checkInQueue() {
-        return new Queue(CHECK_IN_ROUTING_KEY, true);
-    }
+    // --- 2. Filas ---
 
     @Bean
-    public Queue cancellationQueue() {
-        return new Queue(CANCELLATION_QUEUE, true);
-    }
-
-    @Bean
-    public Queue delayQueue() {
-        Map<String, Object> args = new HashMap<>();
-
-        args.put("x-dead-letter-exchange", SCHEDULING_EXCHANGE);
-        args.put("x-dead-letter-routing-key", CHECK_IN_ROUTING_KEY);
-
-        return QueueBuilder.durable(DELAY_QUEUE)
-                .withArguments(args)
+    public Queue standbyDelayQueue() {
+        return QueueBuilder.durable(STAND_BY_DELAY_QUEUE)
+                .withArgument("x-dead-letter-exchange", STAND_BY_DLX_EXCHANGE)
+                .withArgument("x-dead-letter-routing-key", STAND_BY_CANCEL_ROUTING_KEY)
+                .withArgument("x-message-ttl", STAND_BY_TTL_MS)
                 .build();
     }
 
     @Bean
-    public Binding checkInBinding() {
-        return BindingBuilder.bind(checkInQueue())
-                .to(schedulingExchange())
-                .with(CHECK_IN_ROUTING_KEY);
+    public Queue standbyCancelQueue() {
+        return QueueBuilder.durable(STAND_BY_CANCEL_QUEUE).build();
+    }
+
+    // --- 3. Bindings ---
+
+    @Bean
+    public Binding standbyDelayBinding() {
+        return BindingBuilder.bind(standbyDelayQueue())
+                .to(standbyDelayExchange())
+                .with(STAND_BY_DELAY_ROUTING_KEY).noargs();
     }
 
     @Bean
-    public Binding cancellationBinding() {
-        return BindingBuilder.bind(cancellationQueue())
-                .to(schedulingExchange())
-                .with(CANCELLATION_QUEUE);
-    }
-
-    @Bean
-    public Binding delayBinding() {
-        return BindingBuilder.bind(delayQueue())
-                .to(delayExchange())
-                .with(CANCELLATION_QUEUE);
+    public Binding standbyCancelBinding() {
+        return BindingBuilder.bind(standbyCancelQueue())
+                .to(standbyDlxExchange())
+                .with(STAND_BY_CANCEL_ROUTING_KEY).noargs();
     }
 }
